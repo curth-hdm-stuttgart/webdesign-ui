65990e6fdaa5572714b96f7dd7bb054761b5259d
curth-hdm-stuttgart <curth@hdm-stuttgart.de>
2022-01-13 17:17:28 +0100

    Update nach Vorlesung

--- 99_Beispiele/Parallax/Scrolling_Indicator/Scrolling_Indicator.js
+++ 99_Beispiele/Parallax/Scrolling_Indicator/Scrolling_Indicator.js
@@ -5,18 +5,22 @@ function registriereHandler()
 
 function zeichneScrollBar() {
     
-    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;        //  Je nach Browser
-    //console.log("scrollTop body: " + document.body.scrollTop);
-    //console.log("scrollTop docel:" + document.documentElement.scrollTop);
+    const winScroll = document.documentElement.scrollTop;        //  Je nach Browser:document.body.scrollTop || 
+  //  console.log("scrollTop:" + document.documentElement.scrollTop);
     
     //  Die Höhe des Dokuments, gemessen an der Bildschirmkante
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/clientHeight
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    console.log("Doc Height" + document.documentElement.scrollHeight);
-
+  //  console.log("Doc Height" + document.documentElement.scrollHeight);
+    console.log("Client Height" + document.documentElement.clientHeight);
+    
     //  Der prozentuale Wert der Scrollposition
     const scrolled = (winScroll / height) * 100;
     console.log("Scrolled " + scrolled + "%");
+
+
+
+
     document.documentElement.style.setProperty('--scroll',  `${scrolled}%`);
 } 
--- 99_Beispiele/Parallax/Slide_Animation/app.js
+++ 99_Beispiele/Parallax/Slide_Animation/app.js
@@ -2,7 +2,7 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 //  Einblenden des "create Innovation for everyday people text"
-tl.to(".text", { y: "0%", duration: 2, stagger: 0.25 });
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
 
 //  Ein Slider, der einfach so durchläuft
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
@@ -11,7 +11,7 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 
 //  Navigation einblenden (dauert 2 Sekunden)
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 2 });
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 5 });
 
 //  Grossen Text "Stay afloat" einblenden (dauert 2 Sekunden)
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 2 }, "-=1");
